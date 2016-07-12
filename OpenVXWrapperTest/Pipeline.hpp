56553cfad284727ca8918fdc6c57571c7b3d5277
unknown <bilic@A769.schleissheimer.de>
2016-07-12 11:04:29 +0200

    vergessen :)

---
+++ OpenVXWrapperTest/Pipeline.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "spdlog\spdlog.h"
+
+//#include <QApplication>
+#include <QtWidgets/QApplication>
+
+#include <OpenVXWrapper.h>
+#include "ModuleLoader.hpp"
+
+/*Modules*/
+#include "Plugin1.hpp"
+#include "GraphBuilder.h"
+#include "ENC_CudaInterop.hpp"
+#include "VideoGrabberSimu.hpp"
+#include "IMP_CropFrames.hpp"
+#include "IMP_MergeFrames.hpp"
+#include "IMP_ConvColorFrames.hpp"
+#include "DEC_Intel.hpp"
+#include "DEC_inputs.h"
+#include "VPL_FrameProcessor.hpp"
+#include "VPL_Viewer.hpp"
+
+#include "HighResolution\HighResClock.h"
+#include "common\inc\dynlink_cuda.h"
+#include "opencv2\cudev\common.hpp"
+
+#define SAFE_DELETE(P) {if (P) {delete P; P = nullptr;}}
+#define TRACE 1
+#define TRACE_PERFORMANCE
+
+typedef struct stPipelineParams
+{
+    std::shared_ptr<spdlog::logger> file_logger;
+    RW::VPL::VPL_Viewer *pViewer;
+    //VideoPlayer *pViewer;
+
+}tstPipelineParams;
+
+typedef struct stPayloadMsg
+{
+    uint32_t u32Timestamp;
+    uint32_t u32FrameNbr;
+    uint8_t  u8CANSignal1;
+    uint8_t  u8CANSignal2;
+    uint8_t  u8CANSignal3;
+}tstPayloadMsg;
+
+
+class CPipeline : public QObject
+{
+    Q_OBJECT
+public:
+    CPipeline(tstPipelineParams *params);
+    ~CPipeline(){};
+
+public slots:
+    int RunPipeline();
+
+private:
+    tstPipelineParams *m_params;
+};
+
+class CPipethread : public QThread
+{
+    Q_OBJECT
+public:
+    CPipethread(){};
+    ~CPipethread(){};
+    void start();
+
+signals:
+    int started();
+
+};
