370bd2e26e6a48b4a673bb944028247916376a89
Armin.Bilic-EXT <Bilic@schleissheimer.de>
2016-10-19 18:15:27 +0200

    Streaming GUI inkludiert. Kleine Anpassungen an der GUI (Video Tab fehlte wenn man sich als Operator verbindet). (Jetzt richtig eingecheckt)

---
+++ RSAPP/mainwindow.h
@@ -0,0 +1,59 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QListWidgetItem>
+#include "appservice.h"
+
+
+#include "QT_simple\VPL_FrameProcessor.hpp"
+#include "QT_simple\VPL_Viewer.hpp"
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+
+    void on_connectButton_clicked();
+
+    void on_serversList_currentRowChanged(int currentRow);
+
+    void on_usersList_itemClicked(QListWidgetItem *item);
+
+    void on_checkAllBox_toggled(bool checked);
+
+    void on_approveButton_clicked();
+
+    void on_kickButton_clicked();
+
+    void on_saveButton_clicked();
+
+    void on_serversList_itemClicked(QListWidgetItem *item);
+
+private:
+    Ui::MainWindow *ui;
+	RW::VPL::QT_SIMPLE::VPL_Viewer *pViewer;
+    AppService m_appService;
+    bool m_isConnected;
+    bool m_isOperator;
+
+    void FillParameters();
+    void FillUsers();
+    void UpdateUI();
+    void Disconnect();
+    void DisconnectAndClear();
+    void UpdateTabVisibility();
+    QStringList GetSelectedUsers();
+};
+
+#endif // MAINWINDOW_H
